ddc239ddc52b898d3e8970bde7668915e1a30f11
wooix <wooix.pak@gmail.com>
2024-12-21 20:38:07 +0900

    initial commit

---
+++ .setup/.skel/.vimrc
@@ -0,0 +1,18 @@
+"tab설정
+set expandtab       " Tab을 space로 변환
+set tabstop=4       " Tab 키를 눌렀을 때 4개의 space로 설정
+set shiftwidth=4    " 들여쓰기를 할 때 4개의 space로 설정
+set softtabstop=4   " 백스페이스로 4개의 space를 삭제하도록 설정
+
+"검색
+set hlsearch      " 검색 결과를 하이라이트
+set incsearch     " 입력할 때 실시간으로 검색 결과를 표시
+
+
+"문법
+syntax on              " 문법 강조 활성화
+filetype plugin on     " 파일 유형에 따른 플러그인 로드
+filetype indent on     " 파일 유형에 따른 들여쓰기 활성화
+set autoindent
+set smartindent
+
---
+++ .setup/setup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "이 스크립트는 관리자 권한이 필요합니다. sudo를 사용하여 실행하세요."
+    echo "예: sudo $0"
+    exit 1
+fi
+
+# 패키지 목록 업데이트
+sudo apt update
+
+checkstall() {
+    if dpkg -l | grep -qw ${1}; then
+        echo "${1}가 이미 설치되어 있습니다."
+    else
+        echo "${1}가 설치되어 있지 않습니다. 설치를 진행합니다."
+    fi
+
+
+    # ${1} 설치
+    sudo apt install -y ${1}
+
+    # 설치 성공 여부 확인
+    if dpkg -l | grep -qw ${1}; then
+        echo "${1}가 성공적으로 설치되었습니다."
+    else
+        echo "${1} 설치에 실패했습니다. 문제가 발생했습니다."
+    fi
+
+}
+
+for sw in cmatrix bat fzf zoxide eza direnv jq 
+do
+    echo $sw
+    checkstall ${sw}
+done
+
+USER_HOME=$(eval echo ~$SUDO_USER)
+echo "#### 사용자 확인 ####"
+echo $SUDO_USER:$USER_HOME
+
+echo "#### Starship 설치 진행 중..."
+curl -sS https://starship.rs/install.sh | sh || { echo "Starship 설치 실패!"; exit 1; }
+echo "Starship 설치 완료!"
+
+
+echo "#### default script생성"
+cp .skel/.bash_wooix .skel/.bash_profile_yadm
+zoxide init bash >> .skel/.bash_profile_yadm
+echo 'eval "$(zoxide init bash)"' >> .skel/.bash_profile_yadm
+
+
+echo "#### default script복사"
+mv .skel/.bash_profile_yadm ${USER_HOME}		##생성되는 파일이라 옮기고 삭제
+cp .skel/.vimrc ${USER_HOME}
+
+chown $SUDO_USER:$SUDO_USER "${USER_HOME}/.bash_profile_yadm"
+chown $SUDO_USER:$SUDO_USER "${USER_HOME}/.vimrc"
+
+echo "#### bashrc reload"
+echo "#yadm으로 설치된 sw의 사용을 위해서 설정파일을 추가한다." >> ${USER_HOME}/.bashrc
+echo 'source ~/.bash_profile_yadm' >> ${USER_HOME}/.bashrc
+sudo -u ${SUDO_USER} bash -c "source ${USER_HOME}/.bashrc"
+
+echo ""
+echo ""
+echo "#########################################################"
+echo " 다시 terminal을 뜨워주시오~"
+echo "#########################################################"
